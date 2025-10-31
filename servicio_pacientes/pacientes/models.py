066023c7da15e240c4fb62ce76bf7232874b07f3
PaulinaCM7 <paulinacm47@gmail.com>
2025-10-31 12:55:15 -0600

    fix: validacion de nss

--- servicio_pacientes/pacientes/models.py
+++ servicio_pacientes/pacientes/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class Paciente(models.Model):
     nombre = models.CharField(max_length=200, verbose_name="Nombre completo")
     fecha_nacimiento = models.DateField(verbose_name="Fecha de nacimiento")
     nss = models.CharField(
-        min_length=11,
         max_length=11,
+        validators=[MinLengthValidator(11), MaxLengthValidator(11)],
         unique=True,
-        verbose_name="Número de Seguridad Social"
+        verbose_name="Número de Seguridad Social",
+        help_text="Debe contener exactamente 11 dígitos"
     )
     email = models.EmailField(unique=True, verbose_name="Correo electrónico")
     password = models.CharField(max_length=255, verbose_name="Contraseña")
