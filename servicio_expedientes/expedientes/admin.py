1cd5d820957fab55735192c557a7d105856e0c75
PaulinaCM7 <paulinacm47@gmail.com>
2025-10-31 12:56:03 -0600

    feat: se agregaron los modelos de ambos ms a sus admin

--- servicio_expedientes/expedientes/admin.py
+++ servicio_expedientes/expedientes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import NotaMedica
 
-# Register your models here.
+
+@admin.register(NotaMedica)
+class NotaMedicaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'id_paciente', 'id_doctor', 'fecha_consulta', 'fecha_creacion']
+    list_filter = ['fecha_consulta', 'fecha_creacion']
+    search_fields = ['id_paciente', 'id_doctor', 'diagnostico', 'tratamiento']
+    readonly_fields = ['fecha_creacion', 'fecha_actualizacion']
+    date_hierarchy = 'fecha_consulta'
+
+    fieldsets = (
+        ('Referencias', {
+            'fields': ('id_paciente', 'id_doctor')
+        }),
+        ('Información de la Consulta', {
+            'fields': ('fecha_consulta', 'diagnostico', 'tratamiento')
+        }),
+        ('Metadatos', {
+            'fields': ('fecha_creacion', 'fecha_actualizacion'),
+            'classes': ('collapse',)
+        }),
+    )
--- servicio_pacientes/pacientes/admin.py
+++ servicio_pacientes/pacientes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Paciente
 
-# Register your models here.
+
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'nss', 'email', 'es_doctor', 'fecha_registro']
+    list_filter = ['es_doctor', 'fecha_registro']
+    search_fields = ['nombre', 'nss', 'email']
+    readonly_fields = ['fecha_registro', 'fecha_actualizacion', 'password']
+
+    fieldsets = (
+        ('Información Personal', {
+            'fields': ('nombre', 'fecha_nacimiento', 'nss', 'email')
+        }),
+        ('Acceso', {
+            'fields': ('password', 'es_doctor')
+        }),
+        ('Fechas', {
+            'fields': ('fecha_registro', 'fecha_actualizacion'),
+            'classes': ('collapse',)
+        }),
+    )
